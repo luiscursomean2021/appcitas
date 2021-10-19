8397cd8b05b07cb655295669e3f3b8654443e609
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-19 12:38:42 +0200

    servidor animal

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,9 +4,14 @@ const dbconfig = require('./database/db');
 const app = express();
 const port = 8080;
 
+//ROUTERS
+
+const animalRoute = require('./Routes/AnimalRoute');
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/animales', animalRoute);
 
 app.get('/', (req, res) => {
     console.log("Funciona en /");
