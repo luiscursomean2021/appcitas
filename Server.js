d240a4cd877554ac2b028b7dffe9437e3aaa79bf
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2021-10-19 12:40:12 +0200

    server.js con conexion a la BBDD

--- Server.js
+++ Server.js
@@ -3,7 +3,14 @@ const cors = require('cors');
 const dbconfig = require('./database/db');
 const app = express();
 const port = 8080;
+const mongoose = require('mongoose');
 
+//Conexion a la BBDD
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(dbconfig.db, { useNewUrlParser: true })
+    .then(x => console.log(`Conexión correcta con Mongo!! El nombre de la BBDD es: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error conectando con Mongo', err))
 
 app.use(cors());
 app.use(express.json());
