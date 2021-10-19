3bb6c5b1a54c314b5f71a5dc32a5d6aa7dc4e3ae
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-19 12:14:46 +0200

    animal v1.0

---
+++ App/src/app/animal/lista/lista.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ListaDataSource, ListaItem } from './lista-datasource';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ListaItem>;
+  dataSource: ListaDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name','raza','edad','tamanio','vacunas','actions'];
+
+  constructor() {
+    this.dataSource = new ListaDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'animal', loadChildren: () => import('./animal/animal.module').then(m => m.AnimalModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
