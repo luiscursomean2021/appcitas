444bcd474b30cad848a3192a86060d53d93cadc5
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-20 09:13:38 +0200

    servicios animales v1

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'Citas', loadChildren: () => import('./citas/citas.module').then(m => m.CitasModule) },
-
-{ path: "usuarios", loadChildren: () => import("./Core/users/users.module").then(m => m.UsersModule) },
-{ path: 'animal', loadChildren: () => import('./animal/animal.module').then(m => m.AnimalModule) }];
+const routes: Routes = [
+  { path: 'Citas', loadChildren: () => import('./citas/citas.module').then(m => m.CitasModule) },
+  { path: 'animal', loadChildren: () => import('./animal/animal.module').then(m => m.AnimalModule) }
+];
 
 
 @NgModule({
