cf709b2e1f33bcd0225642ebaf5b3ad8700c642b
Deiivid <davidnavarrom3@gmail.com>
2021-10-20 12:46:26 +0200

    comit2

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:"chat",
+    path: "chat",
     loadChildren: () => import("./chat/chat.module").then(m => m.ChatModule)
-  }
+  }, { path: 'Citas', loadChildren: () => import('./citas/citas.module').then(m => m.CitasModule) },
+  { path: 'Auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'animal', loadChildren: () => import('./animal/animal.module').then(m => m.AnimalModule) },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
 ];
 
 @NgModule({
