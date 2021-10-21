312a1cabe1bd8ed9bb08bfde15fcbe0c65f79941
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-21 13:34:33 +0200

    feito

--- App/src/app/public/public-routing.module.ts
+++ App/src/app/public/public-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalListComponent } from './animal-list/animal-list.component';
-import { FavoritosComponent } from './favoritos/favoritos.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PublicComponent } from './public.component';
 
-const routes: Routes = [{
-  path: '', component: PublicComponent},
-  { path: 'fav', component: AnimalListComponent },
+const routes: Routes = [
+  { path: '', component: NavbarComponent, children:[
+    { path: 'fav', component: AnimalListComponent },
+  ] },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
