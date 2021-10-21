54f7164b4682bd023b382cbb4ea6a91153a5b33e
Luis <luiscursomean2021@gmail.com>
2021-10-21 12:35:05 +0200

    AC-14 Localize

--- App/src/environments/environment.prod.ts
+++ App/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  urlAnimales:'http://localhost:4000/animales/',
+  urlUsuarios:'http://localhost:4000/usuarios/',
+  urlLogin: 'http://localhost:4000/login',
+  urlRegistro: 'http://localhost:4000/register'
 };
