18eb7ed423ec8f3fc330d392eec47601b605b8c2
Deiivid <davidnavarrom3@gmail.com>
2021-10-21 08:48:11 +0200

    Guard Login y Role

--- App/src/app/Core/Security/login-guard.guard.ts
+++ App/src/app/Core/Security/login-guard.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuardGuard implements CanActivate {
+  constructor(private router:Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      let token = sessionStorage.getItem("token"); //cogemos datos del sesion storage y  comparamos que no sea nulo
+    if(token!=null){
     return true;
+    }
+    return this.router.navigate(["/Auth/login"]); //si es nulo llevame al login
   }
-  
 }
