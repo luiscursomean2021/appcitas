c45926685e384fda8f371028f9a29814c6218bdc
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-21 11:49:43 +0200

    ultima version funcionando de todos

--- App/src/app/public/public.module.ts
+++ App/src/app/public/public.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
+import { MaterialModule } from '../material.module';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    FavoritosComponent,
+    AnimalListComponent
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    MaterialModule
   ]
 })
 export class PublicModule { }
