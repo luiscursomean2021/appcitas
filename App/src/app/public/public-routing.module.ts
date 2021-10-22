ee58745623a9e71dd63680a3a9ee2f55693eda2b
emilioBrizuela <emilio_brizuela@hotmail.com>
2021-10-22 09:26:33 +0200

    el ortera

--- App/src/app/public/public-routing.module.ts
+++ App/src/app/public/public-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   { path: '', component: NavbarComponent, children:[
